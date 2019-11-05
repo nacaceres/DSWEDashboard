4404db5dfdced03127cc6a42d1b73ab232ede709
andresvaron <af.varon@uniandes.edu.co>
2019-11-04 19:22:04 -0500

    Links

--- front/src/Grupo/Semana/Teamwork/Teamwork.jsx
+++ front/src/Grupo/Semana/Teamwork/Teamwork.jsx
@@ -9,9 +9,53 @@ class Teamwork extends React.Component {
       open: false
     };
   }
+  renderList = () => {
+    console.log(this.props.teamwork.tasks);
+    return this.props.teamwork.tasks.map(task => {
+      return (
+        <tr key={"TASK" + task.taskId}>
+          <th scope="row">{task.nombre}</th>
+          <td>{task.startDate}</td>
+          <td>{task.dueDate}</td>
+          <td>{task.createdOn}</td>
+          <td>{task.completedOn}</td>
+          <td>{task.timeLogger}</td>
+          {this.props.usuario.rol === "ESTUDIANTE" ? (
+            <td>CLICK</td>
+          ) : (
+            <div></div>
+          )}
+        </tr>
+      );
+    });
+  };
+
+  renderColMensaje = () => {};
+
   renderBody = () => {
     if (this.state.open) {
-      return <div className="bodyTeamwork">HOLA</div>;
+      return (
+        <div className="bodyTeamwork">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Nombre</th>
+                <th scope="col">Fecha Inicio</th>
+                <th scope="col">Fecha Limite</th>
+                <th scope="col">Fecha Creación</th>
+                <th scope="col">Fecha Entrega</th>
+                <th scope="col">Minutos Registrados</th>
+                {this.props.usuario.rol === "ESTUDIANTE" ? (
+                  <th scope="col">Comentario</th>
+                ) : (
+                  <div></div>
+                )}
+              </tr>
+            </thead>
+            <tbody>{this.renderList()}</tbody>
+          </table>
+        </div>
+      );
     } else {
       return <div></div>;
     }
