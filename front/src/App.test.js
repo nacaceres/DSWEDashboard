66021e8858aa41d0462acf296b40096ead472b3f
Sergio Guzmán Mayorga <s.guzmanm98@gmail.com>
2019-12-09 20:14:10 -0500

    Update App.test.js

--- front/src/App.test.js
+++ front/src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// sguzmanm: Why keep these tests if you are not using them?
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
